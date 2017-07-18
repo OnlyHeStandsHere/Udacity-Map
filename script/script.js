689a28326867d37c9700356614b262eb66614a97
Jesse Maitland <jesse@confirmedautomation.com>
2017-07-17 21:26:53 -0700

    first wikipedia query solved

--- script/script.js
+++ script/script.js
@@ -3,12 +3,41 @@
  */
 
 var locations = [
-    {lat: 49.372485, lng: -123.099508, name: "Grouse Mountain"},
-    {lat: 49.396207, lng: -123.204513, name: "Cypress Mountain"},
-    {lat: 49.365928, lng: -122.948324, name: "Mount Seymour"},
-    {lat: 49.383529, lng: -123.056634, name: "Mount Fromme"},
-    {lat: 49.311927, lng: -123.082117, name: "Lonsdale Quay"}
+    {lat: 49.372485, lng: -123.099508, name: "Grouse Mountain", info: ''},
+    {lat: 49.396207, lng: -123.204513, name: "Cypress Mountain", info: ''},
+    {lat: 49.365928, lng: -122.948324, name: "Mount Seymour", info: ''},
+    {lat: 49.383529, lng: -123.056634, name: "Mount Fromme", info: ''},
+    {lat: 49.311927, lng: -123.082117, name: "Lonsdale Quay", info: ''}
 ];
+
+// returns a wikipedia base url
+var getBaseWikiUrl = function () {
+    return "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="
+};
+
+var formatPageName = function (name) {
+    return name.replace(' ', '%20')
+};
+
+var url = getBaseWikiUrl() + formatPageName(locations[0].name);
+
+$.ajax({
+    url: url,
+    dataType: 'jsonp',
+    success: function (data) {
+        console.log("request success");
+        var pageKey = '';
+        $.each(data.query.pages, function (item) {
+            pageKey = item;
+        });
+        locations[0].info = data.query.pages[pageKey].extract
+        console.log(locations[0].info)
+    },
+    error: function (data) {
+        alert("ajax failure");
+    }
+});
+
 
 // Class to represent a single location and marker
 function Location(data){
